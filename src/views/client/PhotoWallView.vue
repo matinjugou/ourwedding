6de865d302b3d761b9e666a7b82a6cc39df3bd40
huangchao.thss15 <huangchao.thss15@bytedance.com>
2023-05-19 02:19:16 +0800

    fix bug

--- src/views/client/PhotoWallView.vue
+++ src/views/client/PhotoWallView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { onMounted } from 'vue'
 import { usePhotoStore } from '@/stores/photo.js'
 import AddIcon from '@/components/icons/IconAdd.vue'
 import ZoomInOutImage from '@/components/ZoomInOutImage.vue'
@@ -28,10 +27,6 @@ function loadImage() {
   photoWall.getOlderPhoto()
 }
 
-onMounted(() => {
-  loadImage()
-})
-
 function imageSrc(filename) {
   if (process.env.NODE_ENV === 'development') {
     return `https://fetch-image-wedding-service-lbsvieakmw.cn-beijing.fcapp.run/image?filename=${filename}`
@@ -42,7 +37,7 @@ function imageSrc(filename) {
 </script>
 
 <template>
-  <div class="photo-wall" v-infinite-scroll="loadImage" :infinite-scroll-immediate="false">
+  <div class="photo-wall" v-infinite-scroll="loadImage" :infinite-scroll-distance="100">
     <div class="waterfall-half">
       <ZoomInOutImage
         v-for="(photo, index) in odd(photoWall.photoList)"
