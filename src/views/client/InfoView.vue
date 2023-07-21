6d1c12a4e9006989e85d14314ffa4b23fd866c73
huangchao.thss15 <huangchao.thss15@bytedance.com>
2023-07-21 22:39:15 +0800

    change dress code image

--- src/views/client/InfoView.vue
+++ src/views/client/InfoView.vue
@@ -47,9 +47,12 @@ const guestInfo = useGuestInfoStore()
       <p>1. <span class="important">谢绝礼金，感谢理解</span></p>
       <p>2. 到场后扫描现场二维码签到并领取伴手礼</p>
       <p>3. 如有停车需要请提前联系新郎或者新娘</p>
-      <p>4. 若有条件，着装配色请尽量参考下面的色卡或以简约为主</p>
+      <p>
+        4. 若有条件，着装配色请尽量参考下面的色卡。
+        <span class="important">得体即可，无风格要求。</span>
+      </p>
       <div>
-        <img src="@/assets/images/dress_code.jpeg" style="width: 70%" />
+        <img src="@/assets/images/dress_code.png" style="width: 70%" />
       </div>
     </el-card>
   </div>
